28f19b1f4650b8ea5e386987c4e2ce10d4a901b2
Aayush <khadkaaayush@proton.me>
2025-01-28 21:26:33 +0545

    Add middleware for JSON and URL-encoded request handling, and set up articles route

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ import cors from "cors";
 
 const app = express();
 
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
+
+//routes import
+import articles from "./routes/articles.routes.js";
+
+//routes Declaration
+app.use("/api/v1/articles", articles);
 
 export default app;
