9ae05c71d277c239f02677657872cca7e6213396
Aayush <khadkaaayush@proton.me>
2025-01-28 21:26:43 +0545

    Add controller for retrieving articles with error handling

---
+++ controllers/articles.controllers.js
@@ -0,0 +1,16 @@
+import { Article } from "../models/article.model.js";
+import { asynchandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+
+const getArticles = asynchandler(async (req, res) => {
+  const articles = await Article.find({}).sort({ scraptedAt: -1 }).exec();
+  if (!articles || articles.length === 0) {
+    throw new ApiError(404, "No articles found!");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, articles, "Successfully retrieved articles!"));
+});
+
+export { getArticles };
