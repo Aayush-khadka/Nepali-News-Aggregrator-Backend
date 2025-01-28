5391b5b462232773b3c26beeed435922596ff54d
Aayush <khadkaaayush@proton.me>
2025-01-28 12:10:09 +0545

    Add Rising Nepal scraper and update scraping sequence for improved article extraction

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 import dotenv from "dotenv";
 import Connect_Db from "./db/index.js";
 import { scrapeKathmanduPost } from "./Scraper/kathmanduPost.scraper.js";
+import { scrapeRisingNepal } from "./Scraper/risingNepal.scraper.js";
 import app from "./app.js";
+
 dotenv.config();
+
 Connect_Db()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server Listening at : ${process.env.PORT}`);
     });
+
+    scrapeRisingNepal()
+      .then(() => {
+        console.log("Finished Scraping The Rising Nepal !!!");
+        scrapeKathmanduPost()
+          .then(() => {
+            console.log("Finished Scraping The Kathmandu Post !!!");
+          })
+          .catch((err) => {
+            console.log("ERROR IN SCRAPING The Kathmandu Post: ", err);
+          });
+      })
+      .catch((err) => {
+        console.log("ERROR IN SCRAPING The Rising Nepal: ", err);
+      });
   })
   .catch((err) => {
     console.log("ERROR IN DATABASE: ", err);
-  });
-
-scrapeKathmanduPost()
-  .then(() => {
-    console.log("Finished Scraping The Kathmandu Post !!!");
-  })
-  .catch((err) => {
-    console.log("ERROR IN SCRAPING ", err);
   });
