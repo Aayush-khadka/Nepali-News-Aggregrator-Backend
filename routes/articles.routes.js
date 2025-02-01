3a6fb1bbed71892a58b9b3996a273c2913dbd8b2
Aayush <khadkaaayush@proton.me>
2025-02-01 14:17:56 +0545

    Add trending articles route to articles API

--- routes/articles.routes.js
+++ routes/articles.routes.js
@@ -3,10 +3,12 @@ import {
   getSpecificArticle,
 } from "../controllers/articles.controller.js";
 import { Router } from "express";
+import { getTrending } from "../controllers/trending.controller.js";
 
 const router = Router();
 
 router.route("/get-articles").get(getArticles);
 router.route("/get-article/:id").get(getSpecificArticle);
+router.route("/trending").get(getTrending);
 
 export default router;
