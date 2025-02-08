09416432c5499ebc674c8200316e8e33bd4b692d
Aayush <khadkaaayush@proton.me>
2025-02-08 22:32:54 +0545

    Add search route for articles retrieval

--- app.js
+++ app.js
@@ -18,10 +18,12 @@ app.use(
 import articles from "./routes/articles.routes.js";
 import categories from "./routes/categories.routes.js";
 import bySource from "./routes/bySource.routes.js";
+import search from "./routes/search.routes.js";
 
 //routes Declaration
 app.use("/api/v1/articles", articles);
 app.use("/api/v1/category", categories);
 app.use("/api/v1/by-source/", bySource);
+app.use("/api/v1/search", search);
 
 export default app;
