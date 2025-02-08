6de78d212356231668ac03df5a123e6a6c8a0659
Aayush <khadkaaayush@proton.me>
2025-02-08 14:03:22 +0545

    Implement pagination for articles retrieval with next and previous page links

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -4,8 +4,27 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 
 const getArticles = asynchandler(async (req, res) => {
-  const articles = await Article.find({}).sort({ scraptedAt: -1 }).exec();
-  if (!articles || articles.length === 0) {
+  const page  =  parseInt(req.query.page)||1;
+  const limit  =  parseInt(req.query.limit)||10;
+  const articles = {}
+ articles.results =  await Article.find({}).sort({ scraptedAt: -1 }).skip((page-1)*limit).limit(limit).exec() ;
+
+ const totalDocuments = await Article.countDocuments()
+
+if(page*limit<totalDocuments){
+
+  articles.next = {
+    page:page+1,
+    limit: limit
+  }
+}
+  if(page < 1){
+    articles.previous = {
+      page:page> 1 ? page-1 : null,
+      limit:limit
+    }
+  }
+  if (!articles.results || articles.results.length === 0) {
     throw new ApiError(404, "No articles found!");
   }
   return res
