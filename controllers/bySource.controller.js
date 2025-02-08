0332d2f85927f76d67be30cfc86796d234dcd626
Aayush <khadkaaayush@proton.me>
2025-02-08 15:07:28 +0545

    Implement pagination for articles retrieval from Kathmandu Post and Rising Nepal

--- controllers/bySource.controller.js
+++ controllers/bySource.controller.js
@@ -4,11 +4,30 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 
 const getArticlesFromKathmanduPost = asynchandler(async (req, res) => {
-  const articles = await Article.find({
-    source: "The Kathmandu Post",
-  });
+  const page = parseInt(req.params.page) || 1;
+  const limit = parseInt(req.params.limit) || 10;
 
-  if (!articles || articles.lenght == 0) {
+  const articles = {};
+  articles.result = await Article.find({
+    source: "The Kathmandu Post",
+  })
+    .skip((page - 1) * limit)
+    .limit(limit);
+
+  const totalDocuments = await Article.countDocuments();
+  if (page * limit < totalDocuments) {
+    articles.next = {
+      page: page + 1,
+      limit: limit,
+    };
+  }
+  if (page < 1) {
+    articles.previous = {
+      page: page - 1,
+      limit: limit,
+    };
+  }
+  if (!articles.result || articles.result.length == 0) {
     throw new ApiError(404, "NO Articles from The Kathmandu Post");
   }
 
@@ -24,11 +43,28 @@ const getArticlesFromKathmanduPost = asynchandler(async (req, res) => {
 });
 
 const getArticlesFromRisingNepal = asynchandler(async (req, res) => {
-  const articles = await Article.find({
+  const page = parseInt(req.params.page) || 1;
+  const limit = parseInt(req.params.limit) || 10;
+  const articles = {};
+  articles.result = await Article.find({
     source: "The Rising Nepal",
-  });
-
-  if (!articles || articles.lenght == 0) {
+  })
+    .skip((page - 1) * limit)
+    .limit(limit);
+  const totalDocuments = await Article.countDocuments();
+  if (page * limit < totalDocuments) {
+    articles.next = {
+      page: page + 1,
+      limit: limit,
+    };
+  }
+  if (page < 1) {
+    articles.previous = {
+      page: page - 1,
+      limit: limit,
+    };
+  }
+  if (!articles.result || articles.result.length == 0) {
     throw new ApiError(404, "NO Articles from The Rising Nepal");
   }
 
