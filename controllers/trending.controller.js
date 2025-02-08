6b5dcbed3b63696ce8b5fd655ab9138d17280aaa
Aayush <khadkaaayush@proton.me>
2025-02-08 23:11:15 +0545

    Refactor trending articles retrieval: remove commented code and streamline fetching logic

--- controllers/trending.controller.js
+++ controllers/trending.controller.js
@@ -4,7 +4,6 @@ import { asynchandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const getTrending = asynchandler(async (req, res) => {
-  // Fetch trending titles
   const trendingTitles = await Trending.find({}, { title: 1 }).lean();
   if (!trendingTitles.length) {
     return res
@@ -12,16 +11,13 @@ const getTrending = asynchandler(async (req, res) => {
       .json(new ApiResponse(200, [], "No trending articles found!"));
   }
 
-  // Extract title list
   const titles = trendingTitles.map((article) => article.title);
 
-  // Fetch matching articles in one query
   let trendingArticles = await Article.find({ title: { $in: titles } }).lean();
 
-  // If less than 10 articles were found, fill with other recent articles
   if (trendingArticles.length < 10) {
     const additionalArticles = await Article.find({ title: { $nin: titles } })
-      .sort({ createdAt: -1 }) // Get the most recent articles
+      .sort({ createdAt: -1 })
       .limit(10 - trendingArticles.length)
       .lean();
 
