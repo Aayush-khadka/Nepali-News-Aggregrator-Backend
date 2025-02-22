a98a5de7c343f61d33334a0d43fc725a1f686699
Aayush <khadkaaayush@proton.me>
2025-02-22 20:47:21 +0545

    Remove unused trending and newsletter routes from articles.routes.js

--- routes/articles.routes.js
+++ routes/articles.routes.js
@@ -4,15 +4,11 @@ import {
   getSpecificArticle,
 } from "../controllers/articles.controller.js";
 import { Router } from "express";
-import { getTrending } from "../controllers/trending.controller.js";
-import { getNewsletter } from "../controllers/newsletter.controller.js";
 
 const router = Router();
 
 router.route("/get-latest-articles").get(getLatestArticles);
 router.route("/get-article/:id").get(getSpecificArticle);
-router.route("/trending").get(getTrending);
 router.route("/count").get(getArticleCount);
-router.route("/newsletter").get(getNewsletter);
 
 export default router;
